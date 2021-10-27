bea448d28a7cf53f49e490f89e61c5bcaf2c3cf6
DukeManh <manhducdkcb@gmail.com>
2021-10-27 04:42:08 -0400

    Build static doc site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,14 +8,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://dukemanh.github.io',
+  baseUrl: '/award-winning-doc/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'dukemanh', // Usually your GitHub org/user name.
+  projectName: 'award-winning-doc', // Usually your repo name.
+  trailingSlash: false,
   presets: [
     [
       '@docusaurus/preset-classic',
